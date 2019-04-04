191780a6b436701459958315c22d667eb76efe25
Bram Houben <410994@student.fontys.nl>
2019-04-04 10:56:56 +0200

    registratie

--- ProjectinternDB/Models/ApplicationUser.cs
+++ ProjectinternDB/Models/ApplicationUser.cs
@@ -8,5 +8,8 @@ namespace ProjectinternDB.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
     }
 }

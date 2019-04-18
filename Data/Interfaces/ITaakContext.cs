e3b8e631846f468ff974a43b1fc93e488a02e34a
Rob van der Heijden <413117@student.fontys.nl>
2019-04-18 11:39:30 +0200

    Begon opzet taken aanmaken blokeigenaar
    Voor codereview, hoop dat het niet al te slecht is

---
+++ Data/Interfaces/ITaakContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Model;
+using Model.Onderwijsdelen;
+
+namespace Data.Interfaces
+{
+    public interface ITaakContext
+    {
+        List<Taak> TakenOphalen();
+
+        void TaakToevoegen(Taak taak);
+        
+        void TaakVerwijderen(int taakId);
+
+        List<Traject> GetTrajecten();
+
+        List<Eenheid> GetEenhedenByTrajectId(int trajectId);
+
+        List<Onderdeel> GetOnderdelenByEenheidId(int eenheidId);
+
+        List<Taak> GetTakenByOnderdeelId(int onderdeelId);
+    }
+}
---
+++ Model/Blokeigenaar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Blokeigenaar : Medewerker
+    {
+        public Blokeigenaar(int blokeigenaarId)
+        {
+            BlokeigenaarId = blokeigenaarId;
+        }
+
+        public Blokeigenaar()
+        {
+
+        }
+
+        public int BlokeigenaarId { get; set; }
+    }
+}

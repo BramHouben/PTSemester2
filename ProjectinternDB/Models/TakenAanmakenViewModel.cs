cf4aaa3a0a243011150890c46511f67fc2aa7949
Bram Houben <410994@student.fontys.nl>
2019-04-18 13:48:29 +0200

    Voorkeur

--- ProjectinternDB/Models/TakenAanmakenViewModel.cs
+++ ProjectinternDB/Models/TakenAanmakenViewModel.cs
@@ -9,15 +9,15 @@ namespace ProjectinternDB.Models
 {
     public class TakenAanmakenViewModel
     {
-        [Required]
-        public List<Onderwijstraject> OnderwijsTrajecten { get; set; }
+        //[Required]
+        //public List<Onderwijstraject> OnderwijsTrajecten { get; set; }
 
-        [Required]
-        public List<Onderwijseenheid> OnderwijsEenheden { get; set; }
+        //[Required]
+        //public List<Onderwijseenheid> OnderwijsEenheden { get; set; }
 
-        [Required]
-        public List<Onderwijsonderdeel> OnderwijsOnderdelen { get; set; }
+        //[Required]
+        //public List<Onderwijsonderdeel> OnderwijsOnderdelen { get; set; }
 
-        public List<Onderwijstaak> OnderwijsTaken { get; set; }
+        //public List<Onderwijstaak> OnderwijsTaken { get; set; }
     }
 }

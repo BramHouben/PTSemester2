e9eea7922bf0a368f64463d294b8cf09e2b9d99e
Rob van der Heijden <413117@student.fontys.nl>
2019-06-03 13:25:29 +0200

    MedewerkerSQLContext gebruikt nu using

--- Data/Context/MedewerkerSQLContext.cs
+++ Data/Context/MedewerkerSQLContext.cs
@@ -1,4 +1,5 @@
-﻿using Data.Interfaces;
+﻿using System;
+using Data.Interfaces;
 using Model;
 using Model.Onderwijsdelen;
 using System.Data.SqlClient;
@@ -13,30 +14,40 @@ namespace Data.Context
 
         public Medewerker GetMedewerkerId(string id)
         {
-            conn = dbconn.GetConnString();
-            conn.Open();
-            var cmd = new SqlCommand("SELECT id, Email FROM AspNetUsers where id = @id", conn);
-           
-            cmd.Parameters.AddWithValue("@id", id);
-            var reader = cmd.ExecuteReader();
-
             Medewerker medewerker_new = new Medewerker();
-
-            while (reader.Read())
+            try
             {
-                medewerker_new.MedewerkerId = (string)reader["id"];
-                medewerker_new.Email = (string)reader["Email"];
+                using (SqlConnection conn = dbconn.GetConnString())
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT id, Email FROM AspNetUsers WHERE id = @id", conn),
+                        cmd2 = new SqlCommand("SELECT roleId FROM AspNetUserRoles WHERE UserId = @id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd2.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                medewerker_new.MedewerkerId = (string)reader["id"];
+                                medewerker_new.Email = (string)reader["Email"];
+                            }
+                        }
+                        using (SqlDataReader reader2 = cmd2.ExecuteReader())
+                        {
+                            while (reader2.Read())
+                            {
+                                medewerker_new.Role_id = (string)reader2["roleId"];
+                            }
+                        }
+                    }
+                }
             }
-            reader.Close();
-            var cmd2 = new SqlCommand("select roleId from AspNetUserRoles where UserId = @id", conn);
-            cmd2.Parameters.AddWithValue("@id", id);
-            var reader2 = cmd2.ExecuteReader();
-            while (reader2.Read())
+            catch (Exception e)
             {
-                medewerker_new.Role_id = (string)reader2["roleId"];
+                Console.WriteLine(e);
+                return medewerker_new;
             }
-
-            conn.Close();
             return medewerker_new;
         }
 

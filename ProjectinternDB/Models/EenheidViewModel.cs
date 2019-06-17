51c439687a7bad043a53e185f7124ebf54174a30
Frits van Lieshout <I412791@fhict.nl>
2019-06-17 10:07:41 +0200

    Werkt nog niet, Sander moet er even na kijken ;)

---
+++ ProjectinternDB/Models/EenheidViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectinternDB.Models
+{
+    public class EenheidViewModel
+    {
+        [FromQuery(Name = "ECTS")]
+        public int ECTS { get; set; }
+
+        [FromQuery(Name = "AantalKlassen")]
+        public int AantalKlassen { get; set; }
+    }
+}

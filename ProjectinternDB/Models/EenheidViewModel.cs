0e7a3d5746314b840fd326da0ea94c90578efc32
Bram Houben <410994@student.fontys.nl>
2019-06-19 13:28:22 +0200

    algo

--- ProjectinternDB/Models/EenheidViewModel.cs
+++ ProjectinternDB/Models/EenheidViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,11 @@ namespace ProjectinternDB.Models
     public class EenheidViewModel
     {
         [FromQuery(Name = "ECTS")]
+        [Range(Int32.MinValue, 0, ErrorMessage = "Mag niet lager als nul")]
         public int ECTS { get; set; }
 
-        [FromQuery(Name = "AantalKlassen")]
+        [FromQuery(Name = "Aantal Klassen")]
+        [Range(Int32.MinValue, 0,ErrorMessage ="Mag niet lager als nul")]
         public int AantalKlassen { get; set; }
     }
 }
